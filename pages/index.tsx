d1d96e0d7899a0237f38bf3f2a994cd2dd1fc5e1
mpreter <dshabelyan@mail.ru>
2021-10-07 20:02:51 +0300

    added index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,2 @@
-import Sample from '../components/sample';
-import Buttonui from '../components/sample-mui';
-
-function HomePage() {
-  return (
-    <>
-      <Buttonui />
-      <Sample name="Hello, world!" />
-    </>
-  );
-}
-
-export default HomePage;
+import React from "react";
+import { ReactDOM } from "react";
