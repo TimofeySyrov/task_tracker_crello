38e6705f295868f408d137f16963e40ea7e7a8e9
mpreter <dshabelyan@mail.ru>
2021-10-11 11:30:07 +0300

    Linked style.css and create header

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+function Header({Component, pageProps}:any) {
+
+    return <Component {...pageProps} />
+}
+
+export default Header
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import Navbar from '../components/Header'
 
+function Heading(props:any) {
+  return(
+    <div className="ContainerNavbar">
+      <div className="Navbar">
+        <button className="BtnHome bttn"></button>
+        <button className="MyBoards bttn">My Boards</button>
+        <button className="CreateBoard bttn">Create Board</button>
+        <a href="">
+          <img className="logo" width="138" height="45"></img></a>
+        <a href="">
+          <img className="LoginIcon"></img>
+          </a>
+      </div>
+    </div>
+  )
+}
 
 function HomePage() {
-
+  
   return (
-    <>
-    <Navbar />
-    </>
-  );
+    <div>
+      <Heading heading="Header">
+
+      </Heading>
+    </div>
+  )
+
 }
 
 export default HomePage;
