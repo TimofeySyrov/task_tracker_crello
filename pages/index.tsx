c396b9e222927fcc37162621db32054b51e9666b
mpreter <dshabelyan@mail.ru>
2021-10-11 12:40:12 +0300

    finished write style from header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Heading from '../components/Navbar'
+import Content from '../components/Content'
+
 
 export default function HomePage() {
   
   return (
     <div>
       <Heading heading="Heading"/> 
+      <Content></Content>
     </div>
   )
 
