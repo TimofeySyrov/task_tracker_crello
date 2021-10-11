933449fdd1b1a140d54b33dc0c53d706c76f5353
mpreter <dshabelyan@mail.ru>
2021-10-11 12:24:24 +0300

    Create components

---
+++ components/Content.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Heading(props:any) {
+    return(
+        <div className="ContainerContent">
+            <div className="content">
+                <button className="content__authorization bttn">
+                    authorization
+                </button>
+            </div>
+        </div>
+    )
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,18 +4,18 @@ export default function Heading(props:any) {
   return(
     <div className="ContainerNavbar">
       <div className="Navbar">
-        <button className="BtnHome bttn"></button>
-        <button className="MyBoards bttn">My Boards</button>
-        <button className="CreateBoard bttn">Create Board</button>
-        <a href="#" className="brand">
+        <button className="Navbar__BtnHome bttn"></button>
+        <button className="Navbar__MyBoards bttn">My Boards</button>
+        <button className="Navbar__CreateBoard bttn">Create Board</button>
+        <a href="#" className="Navbar__brand">
           {/* <img className="brandImg" width="138" height="32" src="../images/Brand.jpg" alt="brand"/>
            */}
-           
+
           </a>
-        <a href="#" className="LoginIcon">
+        <a href="#" className="Navbar__LoginIcon">
           {/* <img  width="138" height="32" src={Brand} alt="profile"/>
            */}
-           <div className="LoginImg"></div>
+           <div className="Navbar__LoginImg"></div>
         </a>
       </div>
     </div>
