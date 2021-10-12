30b990e9dcdda4792f6110352f9c03d6c8e55c6e
mpreter <dshabelyan@mail.ru>
2021-10-12 16:44:39 +0300

    import navbar and connect with user board

--- components/profileAutorized.tsx
+++ components/profileAutorized.tsx
@@ -1,21 +1,25 @@
 import { Box } from '@mui/material';
 import ProfileCard from './profileCard';
 import UserAvatar from './avatar';
+import Header from '../components/navbar';
 import Button from '@material-ui/core/Button';
 
 
 function AuthProfile() {
   return(
-    <Box sx={{
-          position:'absolute', 
-          top: 220, 
-          display: 'flex'
-          }}>
-        <UserAvatar/>
-        <Box width={380}>
-        <ProfileCard />
-        </Box>
-    </Box>
+    <>
+      <Header />
+      <Box sx={{
+            position:'absolute', 
+            top: 220, 
+            display: 'flex'
+            }}>
+          <UserAvatar/>
+          <Box width={380}>
+          <ProfileCard />
+          </Box>
+      </Box>
+    </>
   )
 }
 
